3a6faa6ad8cff1983de1cd394e43f547ee879921
Ainaz99 <ainaz.eftekhar@gmail.com>
2021-04-21 21:32:57 +0430

    parser added procedures

--- parser.py
+++ parser.py
@@ -32,6 +32,11 @@ class Parser:
         self.get_next_token()
         if self.lookahead == '$': return
 
+    def get_lookahead(self):
+        if self.lookahead_type == 'ID': return 'ID'
+        if self.lookahead_type == 'NUM': return 'NUM'
+        return self.lookahead
+
 
     def match(self, expected_token, parent):
         matched = False
@@ -49,7 +54,7 @@ class Parser:
             print(f'Missing {expected_token} on line {self.line_number}')
 
     def program(self, parent):
-        l = self.lookahead
+        l = self.get_lookahead()
         if l in first['Declaration-list']:
             self.declaration_list(self.add_node('Declaration-list', parent))
             self.match('$', parent)
@@ -61,7 +66,7 @@ class Parser:
             self.program(parent)
 
     def declaration_list(self, parent):
-        l = self.lookahead
+        l = self.get_lookahead()
         if l in first['Declaration']:
             self.declaration(self.add_node('Declaration', parent))
             self.declaration_list(self.add_node('Declaration-list', parent))
@@ -73,7 +78,7 @@ class Parser:
             self.declaration_list(parent)
 
     def declaration(self, parent):
-        l = self.lookahead
+        l = self.get_lookahead()
         if l in first['Declaration-initial']:
             self.declaration_initial(self.add_node('Declaration-initial', parent))
             self.declaration_prime(self.add_node('Declaration-prime', parent))
@@ -85,7 +90,7 @@ class Parser:
             self.declaration(parent)
 
     def declaration_initial(self, parent):
-        l = self.lookahead
+        l = self.get_lookahead()
         if l in first['Type-specifier']:
             self.type_specifier(self.add_node('Type-specifier', parent))
             self.match('ID', parent)
@@ -97,7 +102,7 @@ class Parser:
             self.declaration_initial(parent)
     
     def declaration_prime(self, parent):
-        l = self.lookahead
+        l = self.get_lookahead()
         if l in first['Fun-declaration-prime']:
             self.fun_declaration_prime(self.add_node('Fun-declaration-prime', parent))
         elif l in first['Var-declaration-prime']:
@@ -110,7 +115,7 @@ class Parser:
             self.declaration_prime(parent)   
 
     def fun_declaration_prime(self, parent):
-        l = self.lookahead
+        l = self.get_lookahead()
         if l == '(':
             self.match('(', parent)
             self.params(self.add_node('Params', parent))
@@ -125,7 +130,7 @@ class Parser:
 
 
     def var_declaration_prime(self, parent):
-        l = self.lookahead
+        l = self.get_lookahead()
         if l == ';':
             self.match(';', parent)
         elif l == '[':
@@ -141,7 +146,7 @@ class Parser:
             self.var_declaration_prime(parent) 
 
     def type_specifier(self, parent):
-        l = self.lookahead
+        l = self.get_lookahead()
         if l == 'int':
             self.match('int', parent)
         elif l == 'void':
@@ -154,7 +159,7 @@ class Parser:
             self.type_specifier(parent)  
 
     def params(self, parent):
-        l = self.lookahead
+        l = self.get_lookahead()
         if l == 'int':          
             self.match('int', parent)
             self.match('ID', parent)
@@ -171,8 +176,8 @@ class Parser:
             self.params(parent) 
 
     def param_list_void_abtar(self, parent):
-        l = self.lookahead
-        if self.lookahead_type == 'ID': # ???
+        l = self.get_lookahead()
+        if l == 'ID':
             self.match('ID', parent)
             self.param_prime(self.add_node('Param-prime', parent))
             self.param_list(self.add_node('Param-list', parent)) 
@@ -184,7 +189,7 @@ class Parser:
             self.param_list_void_abtar(parent) 
 
     def param_list(self, parent):
-        l = self.lookahead
+        l = self.get_lookahead()
         if l == ',':
             self.match(',', parent)
             self.param(self.add_node('Param', parent))
@@ -197,7 +202,7 @@ class Parser:
             self.param_list(parent) 
 
     def param(self, parent):
-        l = self.lookahead
+        l = self.get_lookahead()
         if l in first['Declaration-initial']:
             self.declaration_initial(self.add_node('Declaration-initial', parent))
             self.param_prime(self.add_node('Param-prime', parent))
@@ -209,7 +214,7 @@ class Parser:
             self.param(parent) 
 
     def param_prime(self, parent):
-        l = self.lookahead
+        l = self.get_lookahead()
         if l == '[':
             self.match('[', parent)
             self.match(']', parent)
@@ -221,7 +226,7 @@ class Parser:
             self.param_prime(parent)
 
     def compound_stmt(self, parent):
-        l = self.lookahead
+        l = self.get_lookahead()
         if l == '{':
             self.match('{', parent)
             self.declaration_list(self.add_node('Declaration-list', parent))
@@ -231,7 +236,7 @@ class Parser:
             print(f'Missing Compound-stmt on line {self.line_number}') 
 
     def statement_list(self, parent):
-        l = self.lookahead
+        l = self.get_lookahead()
         if l in first['Statement']:
             self.statement(self.add_node('Statement', parent))
             self.statement_list(self.add_node('Statement-list', parent))
@@ -244,7 +249,7 @@ class Parser:
         
 
     def statement(self, parent):
-        l = self.lookahead
+        l = self.get_lookahead()
         if l in first['Expression-stmt']:
             self.expression_stmt(self.add_node('Expression-stmt', parent))
         elif l in first['Compound-stmt']:
@@ -264,8 +269,151 @@ class Parser:
             self.get_next_token()
             self.statement(parent)
 
+    def expression_stmt(self, parent):
+        l = self.get_lookahead()
+        if l in first['Expression']:
+            self.expression(self.add_node('Expression', parent))
+            self.match(';', parent)
+        elif l == 'break':
+            self.match('break', parent)
+            self.match(';', parent)
+        elif l == ';':
+            self.match(';', parent)
+        elif l in follow['Expression-stmt']:  # Expression-stmt -/-> eps
+            print(f'Missing Expression-stmt on line {self.line_number}') 
+        else:
+            print(f'Illegal {l} on line {self.line_number}')
+            self.get_next_token()
+            self.expression_stmt(parent)
+    
+
+    def selection_stmt(self, parent):
+        l = self.get_lookahead()
+        if l == 'if':
+            self.match('if', parent)
+            self.match('(', parent)
+            self.expression(self.add_node('Expression', parent))
+            self.match(')', parent)
+            self.statement(self.add_node('Statement', parent))
+            self.match('else', parent)
+            self.statement(self.add_node('Statement', parent))
+        elif l in follow['Selection-stmt']: # Selection-stmt -/-> eps
+            print(f'Missing Selection-stmt on line {self.line_number}') 
+        else:
+            print(f'Illegal {l} on line {self.line_number}')
+            self.get_next_token()
+            self.selection_stmt(parent)
+
+
+    def iteration_stmt(self, parent):
+        l = self.get_lookahead()
+        if l == 'while':
+            self.match('while', parent)
+            self.match('(', parent)
+            self.expression(self.add_node('Expression', parent))
+            self.match(')', parent)
+            self.statement(self.add_node('Statement', parent))
+        elif l in follow['Iteration-stmt']: # Iteration-stmt -/-> eps
+            print(f'Missing Iteration-stmt on line {self.line_number}') 
+        else:
+            print(f'Illegal {l} on line {self.line_number}')
+            self.get_next_token()
+            self.iteration_stmt(parent)
+
+    def return_stmt(self, parent):
+        l = self.get_lookahead()
+        if l == 'return':
+            self.match('return', parent)
+            self.return_stmt_prime(self.add_node('Return-stmt-prime', parent))
+        elif l in follow['Return-stmt']: # Return-stmt -/-> eps
+            print(f'Missing Return-stmt on line {self.line_number}')
+        else:
+            print(f'Illegal {l} on line {self.line_number}')
+            self.get_next_token()
+            self.return_stmt(parent) 
+
+    def return_stmt_prime(self, parent):
+        l = self.get_lookahead()
+        if l == ';':
+            self.match(';', parent)
+        elif l in first['Expression']:
+            self.expression(self.add_node('Expression', parent))
+            self.match(';', parent)
+        elif l in follow['Return-stmt-prime']: # Return-stmt-prime -/-> eps
+            print(f'Missing Return-stmt-prime on line {self.line_number}')
+        else:
+            print(f'Illegal {l} on line {self.line_number}')
+            self.get_next_token()
+            self.return_stmt_prime(parent)  
+
+    def for_stmt(self, parent):
+        l = self.get_lookahead()
+        if l == 'for':
+            self.match('for', parent)
+            self.match('ID', parent)
+            self.match('=', parent)
+            self.vars(self.add_node('Vars', parent))
+            self.statement(self.add_node('Statement', parent))
+        elif l in follow['For-stmt']: # For-stmt -/-> eps
+            print(f'Missing For-stmt on line {self.line_number}')
+        else:
+            print(f'Illegal {l} on line {self.line_number}')
+            self.get_next_token()
+            self.for_stmt(parent) 
+
+    def vars(self, parent):
+        l = self.get_lookahead()
+        if l in first['Var']:
+            self.var(self.add_node('Var', parent))
+            self.var_zegond(self.add_node('Var-zegond', parent))
+        elif l in follow['Vars']: # Vars -/-> eps
+            print(f'Missing Vars on line {self.line_number}')
+        else:
+            print(f'Illegal {l} on line {self.line_number}')
+            self.get_next_token()
+            self.vars(parent) 
+
+    def var_zegond(self, parent):
+        l = self.get_lookahead()
+        if l == ',':
+            self.match(',', parent)
+            self.var(self.add_node('Var', parent))
+            self.var_zegond(self.add_node('Var-zegond', parent))
+        elif l in follow['Var-zegond']: # Var-zegond -> eps
+            return
+        else:
+            print(f'Illegal {l} on line {self.line_number}')
+            self.get_next_token()
+            self.var_zegond(parent) 
+
+    def var(self, parent):
+        l = self.get_lookahead()
+        if l == 'ID':
+            self.match('ID', parent)
+            self.var_prime(self.add_node('Var-prime', parent))
+        elif l in follow['Var']: # Var -/-> eps
+            print(f'Missing Var on line {self.line_number}')
+        else:
+            print(f'Illegal {l} on line {self.line_number}')
+            self.get_next_token()
+            self.var(parent) 
+
+    def expression(self, parent):
+        l = self.get_lookahead()
+        if l in first['Simple-expression-zegond']:
+            self.simple_expression_zegond(self.add_node('Simple-expression-zegond', parent))
+        elif l == 'ID':
+            self.match('ID', parent)
+            self.B(self.add_node('B', parent))
+        elif l in follow['Expression']: # Expression -/-> eps
+            print(f'Missing Expression on line {self.line_number}')
+        else:
+            print(f'Illegal {l} on line {self.line_number}')
+            self.get_next_token()
+            self.expression(parent) 
+
     def B(self, parent):
-        l = self.lookahead
+        l = self.get_lookahead()
         if l == '=':
             self.match('=', parent)
             self.expression(self.add_node('Expression', parent))
@@ -284,7 +432,7 @@ class Parser:
             self.B(parent)
 
     def H(self, parent):
-        l = self.lookahead
+        l = self.get_lookahead()
         if l == '=':
             self.match('=', parent)
             self.expression(self.add_node('Expression', parent))
@@ -301,6 +449,8 @@ class Parser:
             self.get_next_token()
             self.H(parent)
 
+    
+
 
     def G(self, parent):
         pass
@@ -311,25 +461,9 @@ class Parser:
     def C(self, parent):
         pass
 
-    def expression_stmt(self, parent):
-        pass
-
     
 
-    def selection_stmt(self, parent):
-        pass
-
-    def iteration_stmt(self, parent):
-        pass
-
-    def return_stmt(self, parent):
-        pass
-
-    def for_stmt(self, parent):
-        pass
-
-    def expression(self, parent):
-        pass
+    
 
     def simple_expression_prime(self, parent):
         pass
